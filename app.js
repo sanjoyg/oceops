51b7e9a6df330e0957bd711ecf00642b0e0899fe
Sanjoy Ghosh <sanjoyg@hcl.com>
2016-09-21 11:23:45 +0530

    work

--- app.js
+++ app.js
@@ -153,6 +153,22 @@ intents.matches('StatusIntent', [
 intents.onDefault([
     function (session, args) {
     	session.send("Hi, am OCE Ops Bot, I can deploy or provide you status on D1..D5 or T1..T6 environments!");
+
+    	var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Hero Card")
+                    .subtitle("Space Needle")
+                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+            ]);
+
+          session.send(msg);
+
     }
 ]);
 
